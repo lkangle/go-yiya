aa92a3d6a38197c350014faac40ad1401b3007ac
likangle <me@lkangle.cn>
2024-11-09 22:32:53 +0800

    fix: toolbar调整

--- frontend/src/components/toolbar.vue
+++ frontend/src/components/toolbar.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { PushpinFilled, SettingFilled, MinusOutlined, CloseOutlined} from '@ant-design/icons-vue'
+import { PushpinOutlined, SettingFilled, PushpinFilled, SettingOutlined, MinusOutlined, CloseOutlined} from '@ant-design/icons-vue'
 import { getAppName, isAllowsOnTop, isWindows } from '@utils/platform';
 import { SetIsAllowsOnTop } from '@wailsjs/go/services/systimeService';
 import { WindowMinimise, Quit } from '@wailsjs/runtime/runtime';
@@ -17,7 +17,6 @@ const toggleAllowsTop = () => {
 
     SetIsAllowsOnTop(allowsTop.value)
 }
-
 </script>
 
 <template>
@@ -25,21 +24,34 @@ const toggleAllowsTop = () => {
     id="app-toolbar"
     class="h-38 w-full text-gray-600 bg-white"
     style="--wails-draggable: drag">
-    <div class="h-full flex items-center justify-between px-6">
-        <!-- window的关闭按钮 -->
-        <div :class="['flex space-x-2', {
-            invisible: !isWindows()
-        }]">
-            <div @click="WindowMinimise" class="bar-icon hover:text-yellow-600 hover:bg-gray-100">
-                <MinusOutlined />
+    <div v-if="isWindows()" class="h-full flex items-center justify-between px-8">
+        <!-- 置顶和设置按钮 -->
+        <div class="flex space-x-3">
+            <div @click="toggleAllowsTop" :class="['bar-icon', {
+                actived: allowsTop
+            }]">
+                <PushpinOutlined :class="{ '-rotate-45': allowsTop}"/>
             </div>
-            <div @click="Quit" class="bar-icon hover:text-red-800 hover:bg-gray-100">
-                <CloseOutlined />
+            <div class="bar-icon">
+                <SettingOutlined />
             </div>
         </div>
         <h3>{{ title }}</h3>
-        <!-- 指定和设置按钮 -->
-        <div class="flex space-x-2">
+        <!-- window的关闭按钮 -->
+        <div class="flex space-x-3">
+            <div @click="WindowMinimise" class="bar-icon hover:text-yellow-600">
+                <MinusOutlined />
+            </div>
+            <div @click="Quit" class="bar-icon hover:bg-red-600 hover:text-gray-100">
+                <CloseOutlined />
+            </div>
+        </div>
+    </div>
+
+    <div v-if="!isWindows()" class="h-full flex items-center justify-center relative">
+        <h3>{{ title }}</h3>
+        <!-- 置顶和设置按钮 -->
+        <div class="flex space-x-3 absolute right-8">
             <div @click="toggleAllowsTop" :class="['bar-icon', {
                 actived: allowsTop
             }]">
@@ -55,7 +67,8 @@ const toggleAllowsTop = () => {
 
 <style>
 .bar-icon {
-    @apply text-12 w-21 h-21 flex items-center justify-center cursor-pointer rounded-[5px];
+    @apply text-center text-12 w-22 h-22 flex items-center justify-center cursor-pointer rounded-[5px] hover:bg-gray-100;
+    transition: all linear 100ms;
 
     &.actived {
         @apply bg-indigo-500 text-gray-100;
