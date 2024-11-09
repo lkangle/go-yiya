a547fafb97e2ad4aff5136d996366df73df9b400
likangle <me@lkangle.cn>
2024-11-09 15:12:46 +0800

    fix: 移除ant

--- frontend/src/components/list.vue
+++ frontend/src/components/list.vue
@@ -1,11 +1,12 @@
 <script setup>
 import { ref } from 'vue';
+import empty from './empty.vue';
 
 let isEmpty = ref(true)
 </script>
 
 <template>
     <div :if="isEmpty" class="w-full h-full flex justify-center items-center pb-50">
-        <Empty/>
+        <empty/>
     </div>
 </template>
